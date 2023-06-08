c32ace83c3d715bfa0a832437f14703af3a1bb33
nattapong <nattapong.nan@kkumail.com>
2023-06-08 22:45:31 +0700

    create save user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,32 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
-const createUser = async (res, req, next) => {
-    
+const passwordEncrypt = (password) => {
+  return bcrypt.hash(password, 12);
+};
+
+const createUser = (res, req, next) => {
+  const fname = req.body.firstName;
+  const lname = req.body.lastName;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const passwordEncrypted = passwordEncrypt(password);
+
+  const user = new User({
+    firstName: fname,
+    lastName: lname,
+    username: username,
+    email: email,
+    password: passwordEncrypted,
+  });
+
+  user
+    .save()
+    .then(result => {
+        res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
