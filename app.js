90312810c63b76e646fdc548a663d414015e8fdb
nattapong <nattapong.nan@kkumail.com>
2023-06-08 18:59:57 +0700

    add user routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const Port = process.env.PORT || 3000;
 const Hostname = process.env.HOSTNAME;
 
 const db = require("./db/dbConnection");
@@ -12,8 +12,8 @@ const routes = require("./routes");
 app.use(routes);
 
 db.then(() => {
-  app.listen(PORT, Hostname, () => {
-    console.log(`Server running on port ${PORT}`);
+  app.listen(Port, Hostname, () => {
+    console.log(`Server running on port ${Port}`);
   });
 }).catch(err => {
     console.log(err);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const Blog = require('./blogRoutes');
+const User = require('./userRoutes');
 
 const express = require('express');
 const router = express.Router();
 
 router.use(Blog);
+router.use(User);
 
 module.exports = router;
