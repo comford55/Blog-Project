18967a997170395a284739c887c5779782d12d54
nattapong <nattapong.nan@kkumail.com>
2023-06-09 00:32:10 +0700

    modify userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,20 +3,8 @@ const router = express.Router();
 
 const controllers = require("../controllers");
 
-router.get("/users", (req, res, next) => {
-  res.json({ message: "users" });
-});
-
-// router.post("/user", async (req, res, next) => {
-//   const creatUser = await controllers.userController.createUser(req, res);
-// });
-
 router.post("/user", controllers.userController.createUser);
 
-router.post("/user/:id", controllers.userController.getUserProfile);
-
-// router.post("/user/:id", async (req, res, next) => {
-//     const userProfile = await controllers.userController.getUserProfile(req, res);
-// });
+router.get("/user/:id", controllers.userController.getUserProfile);
 
 module.exports = router;
